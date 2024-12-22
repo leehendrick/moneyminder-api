d3d60a27578562a8af3fe72e0ef6f5d3a706b767
leehendrick <imhendrick16@gmail.com>
2024-12-21 20:57:01 -0500

    create transactions migration

---
+++ database/migrations/2024_12_21_205214_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Category;
+use App\Models\TransactionType;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('value');
+            $table->date('date');
+            $table->string('description')->nullable();
+            $table->foreignId('transaction_type_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
