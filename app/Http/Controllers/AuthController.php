8805095d3d74d708834bd5cf745ea47f54f717ea
leehendrick <imhendrick16@gmail.com>
2024-12-22 00:57:18 -0500

    logging in the user them launch the token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,16 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     use ApiResponses;
+
     public function login(ApiLoginRequest $request) {
-        return $this->ok($request->get('email'));
+        $request->validated();
+
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalide credentials', 401);
+        }
+
+        $user = User::firstWhere('email', $request->email);
+
+        return $this->ok(
+            'Authenticated',
+            [
+                'token' => $user->createToken('API token for ' . $user->email)->plainTextToken,
+            ]
+        );
     }
 
     public function register() {
-        return $this->ok('register');
+
     }
 }
 
