38ca01bf3b90b2e3800eab242496042bbd57e557
leehendrick <imhendrick16@gmail.com>
2024-12-21 23:20:11 -0500

    add relationship with respective models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use \Illuminate\Database\Eloquent\Relations\HasMany;
+use \Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function transactionType(): HasMany
+    {
+        return $this->hasMany(TransactionType::class);
+    }
 }
