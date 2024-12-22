9f5820c6ee414e99a57c57cb1b2d2a9689311f06
leehendrick <imhendrick16@gmail.com>
2024-12-21 22:19:21 -0500

    update user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('default_currency')->default('CAD');
+            $table->boolean('is_notifiable')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
