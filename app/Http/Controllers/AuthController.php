445ff5fcd169badda293c5fafe8e76eb7a078c5f
leehendrick <imhendrick16@gmail.com>
2024-12-22 23:49:37 -0500

    add the expiration time of the token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,10 @@ class AuthController extends Controller
         return $this->ok(
             'Authenticated',
             [
-                'token' => $user->createToken('API token for ' . $user->email)->plainTextToken,
+                'token' => $user->createToken(
+                    'API token for ' . $user->email,
+                    ['*'],
+                    now()->addMonth())->plainTextToken,
             ]
         );
     }
