9192a013f917cb19e5241a3c87c00c9e921b59b2
leehendrick <imhendrick16@gmail.com>
2024-12-23 16:01:56 -0500

    create customized payload

--- app/Http/Resources/V1/TransactionTypeResource.php
+++ app/Http/Resources/V1/TransactionTypeResource.php
@@ -14,6 +14,16 @@ class TransactionTypeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'type' => 'transaction_type',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                $this->mergeWhen($request->routeIs('transaction_types.*'),[
+                    'createdAt' => $this->created_at,
+                    'updatedAt' => $this->updated_at,
+                ])
+            ],
+        ];
     }
 }
