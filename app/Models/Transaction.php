d19a9ae4992d336e03c45fcd09d4954f1c13a97d
leehendrick <imhendrick16@gmail.com>
2024-12-23 15:46:04 -0500

    add relationship between user and transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -20,4 +20,9 @@ class Transaction extends Model
     {
         return $this->hasMany(TransactionType::class);
     }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
