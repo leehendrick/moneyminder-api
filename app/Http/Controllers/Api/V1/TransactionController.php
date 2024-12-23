ed374e15e07483eeec0bdeb40976ab7e1fcdf87d
leehendrick <imhendrick16@gmail.com>
2024-12-23 00:42:16 -0500

    add show function

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\TransactionResource;
 use App\Models\Transaction;
 use App\Http\Requests\Api\V1\StoreTransactionRequest;
 use App\Http\Requests\Api\V1\UpdateTransactionRequest;
@@ -11,6 +12,10 @@ class TransactionController extends Controller
 {
     public function index(){
         return Transaction::all();
+    }
+
+    public function show(Transaction $transaction){
+        return new TransactionResource($transaction);
     }
 }
 
