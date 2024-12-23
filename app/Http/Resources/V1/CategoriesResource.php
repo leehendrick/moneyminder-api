a16ffe66ddc061458f37eaae048a7959c90b8174
leehendrick <imhendrick16@gmail.com>
2024-12-23 16:32:13 -0500

    add customized payloads

--- app/Http/Resources/V1/CategoriesResource.php
+++ app/Http/Resources/V1/CategoriesResource.php
@@ -14,6 +14,37 @@ class CategoriesResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'type' => 'categories',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                $this->mergeWhen($request->routeIs('categories.*'),[
+                    'monthly_limit' => $this->monthly_limit,
+                    'is_default' => $this->is_default,
+                    'createdAt' => $this->created_at,
+                    'updatedAt' => $this->updated_at,
+                ])
+            ],
+            'author' => [
+                'data' => [
+                    'type' => 'user',
+                    'id' => $this->user_id
+                ],
+                'links' => [
+                    [
+                        'self' => 'todo',
+                    ]
+                ],
+            ],
+            'includes' => [
+                new UserResource($this->user)
+            ],
+            'links' => [
+                [
+                    'self' => route('categories.show', ['categories' => $this->id])
+                ]
+            ],
+        ];
     }
 }
