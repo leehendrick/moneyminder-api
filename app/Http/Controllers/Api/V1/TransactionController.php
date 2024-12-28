9900b8c3189ff1a6dcccd4050df333827b63765b
leehendrick <imhendrick16@gmail.com>
2024-12-27 21:07:44 -0500

    condicionando o retorno de user na relação

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -2,16 +2,23 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\TransactionResource;
 use App\Models\Transaction;
-class TransactionController extends Controller
+class TransactionController extends ApiController
 {
     public function index(){
-        return TransactionResource::collection(Transaction::all());
+
+        if ($this->include('author')) {
+            return TransactionResource::collection(Transaction::with('user')->paginate());
+        }
+        return TransactionResource::collection(Transaction::paginate());
     }
 
     public function show(Transaction $transaction){
+
+        if ($this->include('author')) {
+            return new TransactionResource($transaction->load('user'));
+        }
         return new TransactionResource($transaction);
     }
 }
