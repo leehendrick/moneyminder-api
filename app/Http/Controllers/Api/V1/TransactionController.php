c3c4796d5c33db1cd649cb184683c3276af81388
leehendrick <imhendrick16@gmail.com>
2024-12-28 01:15:09 -0500

    update transaction controller

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Filters\V1\TransactionFilter;
 use App\Http\Resources\V1\TransactionResource;
 use App\Models\Transaction;
 class TransactionController extends ApiController
 {
-    public function index(){
-
-        if ($this->include('author')) {
-            return TransactionResource::collection(Transaction::with('user')->paginate());
-        }
-        return TransactionResource::collection(Transaction::paginate());
+    public function index(TransactionFilter $filters){
+        return TransactionResource::collection(Transaction::filter($filters)->paginate());
     }
 
     public function show(Transaction $transaction){
