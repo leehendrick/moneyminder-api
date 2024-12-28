e99f2cc181b63813d5bf72a1b941c2eb755a96bc
leehendrick <imhendrick16@gmail.com>
2024-12-28 00:40:48 -0500

    add scope filter function

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use \Illuminate\Database\Eloquent\Relations\HasMany;
 use \Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Http\Filters\V1\QueryFilter;
 
 class Transaction extends Model
 {
@@ -25,4 +27,9 @@ class Transaction extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters){
+        return $filters->apply($builder);
+    }
+
 }
