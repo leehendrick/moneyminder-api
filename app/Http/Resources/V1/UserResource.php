30d81d0666b75e6aab873cfab875128bc585c5e9
leehendrick <imhendrick16@gmail.com>
2024-12-27 22:18:06 -0500

    update user resource

--- app/Http/Resources/V1/UserResource.php
+++ app/Http/Resources/V1/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,6 +13,8 @@ class UserResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+
+
     public function toArray(Request $request): array
     {
         return [
