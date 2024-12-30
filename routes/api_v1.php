c9984c55b66070fae55bce5eef3e97967c4726ea
leehendrick <imhendrick16@gmail.com>
2024-12-30 12:26:20 -0500

    add user routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -10,6 +10,8 @@ use \App\Http\Controllers\Api\V1\UserTransactionsController;
 
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+
+    Route::apiResource('users', UsersController::class);
     Route::apiResource('transactions', TransactionController::class)->except(['update']);
     Route::put('transactions/{transaction}', [TransactionController::class, 'replace']);
 
