203403d3a10b003bba94a1897b274a351c0f076d
leehendrick <imhendrick16@gmail.com>
2024-12-29 22:04:36 -0500

    add route for user transaction relation

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -17,5 +17,5 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
     Route::middleware('auth:sanctum')->apiResource('transactionTypes', TransactionTypesController::class);
     Route::middleware('auth:sanctum')->apiResource('categories', CategoriesController::class);
-    Route::middleware('auth:sanctum')->apiResource('users.transactions', UserTransactionsController::Controller::class);
+    Route::middleware('auth:sanctum')->apiResource('users.transactions', UserTransactionsController::class);
 });
