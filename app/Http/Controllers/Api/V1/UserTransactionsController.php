4ef7c6ca9cf71582343db237db588ec5d6abe867
leehendrick <imhendrick16@gmail.com>
2024-12-29 20:10:29 -0500

    create index function && add filter

--- app/Http/Controllers/Api/V1/UserTransactionsController.php
+++ app/Http/Controllers/Api/V1/UserTransactionsController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\V1\TransactionFilter;
+use App\Http\Resources\V1\TransactionResource;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class UserTransactionsController extends Controller
 {
-    //
+    public function index($user_id, TransactionFilter $filters)
+    {
+        return TransactionResource::collection(Transaction::where('user_id', $user_id)
+            ->filter($filters)
+            ->paginate());
+    }
 }
