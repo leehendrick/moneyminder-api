08ea731765f362ff5f35867a486c4196865a27a9
leehendrick <imhendrick16@gmail.com>
2024-12-30 00:26:37 -0500

    add store function

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Filters\V1\TransactionFilter;
+use App\Http\Requests\Api\V1\StoreTransactionRequest;
 use App\Http\Resources\V1\TransactionResource;
 use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class TransactionController extends ApiController
 {
     public function index(TransactionFilter $filters){
         return TransactionResource::collection(Transaction::filter($filters)->paginate());
+    }
+
+    public function store(StoreTransactionRequest $request)
+    {
+        try {
+            $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        } catch (ModelNotFoundException $exception){
+            return $this->ok('User not found', [
+                'error' => 'The provided user id does not exist.'
+            ]);
+        }
+
+        $model = [
+            'value' => $request->input('data.attributes.value'),
+            'date' => $request->input('data.attributes.date'),
+            'description' => $request->input('data.attributes.description'),
+            'transaction_type_id' => $request->input('data.relationships.transactionType.data.id'),
+            'category_id' => $request->input('data.relationships.category.data.id'),
+            'user_id' => $request->input('data.relationships.author.data.id'),
+        ];
+
+        return new TransactionResource(Transaction::create($model));
     }
 
     public function show(Transaction $transaction){
