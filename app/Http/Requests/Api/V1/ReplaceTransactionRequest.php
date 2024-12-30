90386268f1a032a7b21300934231109a645814cb
leehendrick <imhendrick16@gmail.com>
2024-12-30 01:54:41 -0500

    create replace transaction request

---
+++ app/Http/Requests/Api/V1/ReplaceTransactionRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReplaceTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'data.attributes.value' => 'required|string',
+            'data.attributes.date' => 'required|date_format:Y-m-d',
+            'data.attributes.description' => 'required|string',
+            'data.relationships.author.data.id' => 'required|integer',
+            'data.relationships.transactionType.data.id' => 'required|integer',
+            'data.relationships.category.data.id' => 'required|integer',
+        ];
+
+        return $rules;
+    }
+
+    public function messages(): array
+    {
+        return  [
+            'data.attributes.date' => 'The provided date is invalid. Please use yyyy-mm-dd format.',
+        ];
+    }
+}
