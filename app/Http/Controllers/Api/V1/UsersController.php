da91c2af29bab0ec5fda75c1415a5f04e7a5c764
leehendrick <imhendrick16@gmail.com>
2024-12-29 23:22:13 -0500

    update directory

--- app/Http/Controllers/Api/V1/UsersController.php
+++ app/Http/Controllers/Api/V1/UsersController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\UserFilter;
-use App\Http\Requests\StoreUserRequest;
-use App\Http\Requests\UpdateUserRequest;
+use App\Http\Requests\Api\V1\StoreUserRequest;
+use App\Http\Requests\Api\V1\UpdateUserRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 
