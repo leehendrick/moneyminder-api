97ef938979cd11bb4a788f61d610436157a5661a
leehendrick <imhendrick16@gmail.com>
2024-12-30 23:22:43 -0500

    add policy for transaction

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Policies\V1\TransactionPolicy;
 use App\Traits\ApiResponses;
-use Illuminate\Http\Request;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ApiController extends Controller
 {
     use ApiResponses;
+    use AuthorizesRequests;
+
+    protected $policyClass = TransactionPolicy::class;
+
     public function include(string $relationship): bool {
 
         $param = request()->get('include');
@@ -20,5 +26,10 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    public function isAble($ability, $targetModel): Response
+    {
+        return $this->authorize($ability, [$targetModel, $this->policyClass]);
     }
 }
