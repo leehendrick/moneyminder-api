3362c39d499ad26a09187b644450e7b36ed4d3e5
leehendrick <imhendrick16@gmail.com>
2025-01-02 03:21:39 -0500

    update

--- app/Http/Requests/Api/V1/StoreTransactionRequest.php
+++ app/Http/Requests/Api/V1/StoreTransactionRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Api\V1;
 
+use App\Permissions\V1\Abilities;
 use Illuminate\Contracts\Validation\ValidationRule;
 
 class StoreTransactionRequest extends BaseTransactionRequest
@@ -25,12 +26,18 @@ class StoreTransactionRequest extends BaseTransactionRequest
             'data.attributes.value' => 'required|string',
             'data.attributes.date' => 'required|date_format:Y-m-d',
             'data.attributes.description' => 'required|string',
+            'data.relationships.author.data.id' => 'required|integer|exists:users,id',
         ];
 
+        $user = $this->user();
+
         if ($this->routeIs('transactions.store')) {
-            $rules ['data.relationships.author.data.id'] = 'required|integer';
             $rules ['data.relationships.transactionType.data.id'] = 'required|integer';
             $rules ['data.relationships.category.data.id'] = 'required|integer';
+
+            if ($user->tokenCan(Abilities::CreateOwnTransaction)) {
+                $rules ['data.relationships.author.data.id'] .= '|size:' . $user->id;
+            }
         }
 
         return $rules;
