35cb0229285f6b9b52114f5f7029958c16412c3e
leehendrick <imhendrick16@gmail.com>
2025-01-18 01:12:30 -0500

    refactor resources

--- app/Http/Controllers/Api/V1/UserTransactionsController.php
+++ app/Http/Controllers/Api/V1/UserTransactionsController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\Api\V1\StoreTransactionRequest;
 use App\Http\Requests\Api\V1\UpdateTransactionRequest;
 use App\Http\Resources\V1\TransactionResource;
 use App\Models\Transaction;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
@@ -23,20 +24,35 @@ class UserTransactionsController extends ApiController
 
     public function store($user_id, StoreTransactionRequest $request)
     {
-        return new TransactionResource(Transaction::create($request->mappedAttributes()));
+        try {
+            $this->isAble('store', Transaction::class);
+
+            return new TransactionResource(Transaction::create($request->mappedAttributes([
+                'user_id' => $user_id,
+            ])));
+
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to create that resource.', 401);
+        }
     }
 
     public function update(UpdateTransactionRequest $request, $user_id, $transaction_id){
         //PATCH
         try {
-            $transaction = Transaction::findOrFail($transaction_id);
+            $transaction = Transaction::where('id', $transaction_id)
+                ->where('user_id', $user_id)
+                ->firstOrFail();
 
-            if ($transaction->user_id == $user_id) {
-                $transaction->update($request->mappedAttributes());
-                return new TransactionResource($transaction);
-            }
+            $this->isAble('update', $transaction);
+
+            $transaction->update($request->mappedAttributes());
+
+            return new TransactionResource($transaction);
+
         } catch (ModelNotFoundException $exception) {
             return $this->error('Transaction can not be found', 404);
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to update that resource.', 401);
         }
     }
 
@@ -44,34 +60,40 @@ class UserTransactionsController extends ApiController
     {
         //PUT
         try {
-            $transaction = Transaction::findOrFail($transaction_id);
+            $transaction = Transaction::where('id', $transaction_id)
+                ->where('user_id', $user_id)
+                ->firstOrFail();
 
-            if($transaction->user_id == $user_id){
+            $this->isAble('replace', $transaction);
 
-                $transaction->update($request->mappedAttributes());
+            $transaction->update($request->mappedAttributes());
 
-                return new TransactionResource($transaction);
-            }
+            return new TransactionResource($transaction);
 
         } catch (ModelNotFoundException $exception){
             return $this->error('Transaction can not be found.', 404);
+        } catch (AuthorizationException $exception){
+            return $this->error('You are not authorized to update that resource', 401);
         }
     }
 
     public function destroy($user_id, $transaction_id)
     {
         try {
-            $transaction = Transaction::findOrFail($transaction_id);
+            $transaction = Transaction::where('id', $transaction_id)
+                ->where('user_id', $user_id)
+                ->firstOrFail();
 
+            $this->isAble('delete', $transaction);
 
-            if($transaction->user_id == $user_id){
-                $transaction->delete();
-                return $this->ok('Transaction sucessfully deleted.');
-            }
+            $transaction->delete();
 
-            return $this->error('Transaction can not be found.', 404);
+            return $this->ok('Ticket sucessfully deleted.');
+
         } catch(ModelNotFoundException $exception) {
             return $this->error('Transaction can not be found.', 404);
+        } catch (AuthorizationException $ex) {
+            return $this->error('You are not authorized to update delete that resource.', 401);
         }
     }
 }
