7cce5689bcfd636451ce7ded502b9f2322f535a6
leehendrick <imhendrick16@gmail.com>
2025-02-01 16:38:32 -0500

    create: base user request

---
+++ app/Http/Requests/Api/V1/BaseUserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseUserRequest extends FormRequest
+{
+
+    public function mappedAttributes(array $otherAttributes = []): array
+    {
+        $attributeMap = array_merge([
+            'data.attributes.name' => 'name',
+            'data.attributes.email' => 'email',
+            'data.attributes.password' => 'password',
+            'data.attributes.emailVerifiedAt' => 'email_verified_at',
+            'data.relationships.default_currency' => 'default_currency',
+            'data.relationships.is_notifiable' => 'is_notifiable',
+        ], $otherAttributes);
+
+        $attributesToUpdate = [];
+
+        foreach ($attributeMap as $key => $attribute) {
+            if ($this->has($key)) {
+
+                $value = $this->input($key);
+
+                //Hash da senha
+                if ($attribute === 'password') {
+                    $value = bcrypt($value);
+                }
+
+                $attributesToUpdate[$attribute] = $value;
+            }
+        }
+
+        return $attributesToUpdate;
+    }
+
+    public function messages(): array
+    {
+        return  [
+            'MSG' => 'MSG',
+        ];
+    }
+}
