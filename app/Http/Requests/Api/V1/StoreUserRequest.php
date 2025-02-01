0cf91828f46bb234a510809b1d5c4d41e57fc964
leehendrick <imhendrick16@gmail.com>
2025-02-01 16:38:59 -0500

    add: user rules for validation

--- app/Http/Requests/Api/V1/StoreUserRequest.php
+++ app/Http/Requests/Api/V1/StoreUserRequest.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Requests\Api\V1;
 
-use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUserRequest extends FormRequest
+class StoreUserRequest extends BaseUserRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +21,11 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'data.attributes.name' => 'required|string',
+            'data.attributes.email' => 'required|string|email|unique:users,email',
+            'data.attributes.password' => 'required|string',
+            'data.relationships.default_currency' => 'string|nullable',
+            'data.relationships.is_notifiable' => 'boolean|nullable',
         ];
     }
 }
