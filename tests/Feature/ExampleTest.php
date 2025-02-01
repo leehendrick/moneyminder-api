6f7caa80eed19a2b6c1d1abfa402342c07fa738c
leehendrick <imhendrick16@gmail.com>
2025-02-01 15:40:43 -0500

    righting tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,3 +5,8 @@ it('returns a successful response', function () {
 
     $response->assertStatus(200);
 });
+
+it('returns an error response', function () {
+    $response = $this->get('/login');
+    $response->assertStatus(404);
+});
