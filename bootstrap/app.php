289283d4280d9af35122b63cd7634327546806c8
leehendrick <imhendrick16@gmail.com>
2025-02-01 15:39:13 -0500

    add: customized error response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +22,52 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ValidationException $e) {
+            foreach ($e->errors() as $key => $value) {
+                foreach ($value as $message) {
+                    $errors[] = [
+                        'status' => $e->status,
+                        'message' => $message,
+                        'source' => $key,
+                    ];
+                }
+            }
+
+            return response()->json([
+                'errors' => $errors,
+            ]);
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e) {
+            $errors[] = [
+                'status' => 404,
+                'message' => $e->getMessage(),
+            ];
+
+            return response()->json([
+                'errors' => $errors,
+            ]);
+        });
+
+        $exceptions->render(function (AccessDeniedHttpException $e) {
+            $errors[] = [
+                'status' => $e->getStatusCode(),
+                'message' => $e->getMessage(),
+            ];
+
+            return response()->json([
+                'errors' => $errors,
+            ]);
+        });
+
+        $exceptions->render(function (AuthenticationException $e) {
+            $errors[] = [
+                'status' => 403,
+                'message' => $e->getMessage(),
+            ];
+
+            return response()->json([
+                'errors' => $errors,
+            ]);
+        });
     })->create();
