ad6793ca65988e7faddae75497c942476d5b6727
leehendrick <imhendrick16@gmail.com>
2025-02-02 01:05:19 -0500

    add: new admin abilities

--- app/Permissions/V1/Abilities.php
+++ app/Permissions/V1/Abilities.php
@@ -15,6 +15,11 @@ final class Abilities {
     public const ReplaceOwnTransaction = 'transaction:own:replace';
     public const DeleteOwnTransaction= 'transaction:own:delete';
 
+    public const CreateTransactionType = 'transaction_type:create';
+    public const UpdateTransactionType = 'transaction_type:update';
+    public const ReplaceTransactionType = 'transaction_type:replace';
+    public const DeleteTransactionType = 'transaction_type:delete';
+
     public const CreateUser = 'user:create';
     public const UpdateUser = 'user:update';
     public const ReplaceUser = 'user:replace';
@@ -23,6 +28,16 @@ final class Abilities {
     public const UpdateOwnUser = 'user:own:update';
     public const ReplaceOwnUser = 'user:own:replace';
     public const DeleteOwnUser = 'user:own:delete';
+
+    public const CreateCategory = 'category:create';
+    public const UpdateCategory = 'category:update';
+    public const ReplaceCategory = 'category:replace';
+    public const DeleteCategory = 'category:delete';
+
+    public const UpdateOwnCategory = 'category:own:update';
+    public const ReplaceOwnCategory = 'category:own:replace';
+    public const DeleteOwnCategory = 'category:own:delete';
+    public const CreateOnwCategory = 'category:own:create';
 
     public static function getAbilities(User $user): array
     {
@@ -33,6 +48,14 @@ final class Abilities {
                 self::UpdateTransaction,
                 self::ReplaceTransaction,
                 self::DeleteTransaction,
+                self::CreateTransactionType,
+                self::UpdateTransactionType,
+                self::ReplaceTransactionType,
+                self::DeleteTransactionType,
+                self::CreateCategory,
+                self::UpdateCategory,
+                self::ReplaceCategory,
+                self::DeleteCategory,
                 self::CreateUser,
                 self::UpdateUser,
                 self::ReplaceUser,
@@ -44,12 +67,14 @@ final class Abilities {
                 self::UpdateOwnTransaction,
                 self::DeleteOwnTransaction,
                 self::ReplaceOwnTransaction,
+                self::CreateOnwCategory,
+                self::UpdateOwnCategory,
+                self::ReplaceOwnCategory,
+                self::DeleteOwnCategory,
                 self::UpdateOwnUser,
                 self::ReplaceOwnUser,
                 self::DeleteOwnUser,
             ];
         }
     }
-
-
 }
