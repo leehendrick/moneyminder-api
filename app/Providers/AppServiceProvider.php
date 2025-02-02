6233ebb15f32f494d5b4361912640ffe1e5fc5b6
leehendrick <imhendrick16@gmail.com>
2025-02-01 20:51:42 -0500

    add:  define gate policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
+use App\Policies\V1\UserPolicy;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('update', [UserPolicy::class, 'update']);
+        Gate::define('replace', [UserPolicy::class, 'replace']);
+        Gate::define('delete', [UserPolicy::class, 'delete']);
     }
 }
