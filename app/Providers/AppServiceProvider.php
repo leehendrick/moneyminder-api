6eac193772e80aa088d99e912cd79f9ff472b506
leehendrick <imhendrick16@gmail.com>
2025-02-02 01:04:49 -0500

    update: define new gate policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 
+use App\Policies\V1\CategoryPolicy;
+use App\Policies\V1\TransactionTypePolicy;
 use Illuminate\Support\ServiceProvider;
 use App\Policies\V1\UserPolicy;
 use Illuminate\Support\Facades\Gate;
@@ -25,5 +27,15 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('update', [UserPolicy::class, 'update']);
         Gate::define('replace', [UserPolicy::class, 'replace']);
         Gate::define('delete', [UserPolicy::class, 'delete']);
+
+        Gate::define('update', [CategoryPolicy::class, 'update']);
+        Gate::define('replace', [CategoryPolicy::class, 'replace']);
+        Gate::define('create', [CategoryPolicy::class, 'create']);
+        Gate::define('delete', [CategoryPolicy::class, 'delete']);
+
+        Gate::define('update', [TransactionTypePolicy::class, 'update']);
+        Gate::define('replace', [TransactionTypePolicy::class, 'replace']);
+        Gate::define('create', [TransactionTypePolicy::class, 'create']);
+        Gate::define('delete', [TransactionTypePolicy::class, 'delete']);
     }
 }
