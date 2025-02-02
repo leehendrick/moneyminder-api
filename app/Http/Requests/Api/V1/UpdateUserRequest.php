f697029eb4e91f5be49f52a841bf38277e25cf92
leehendrick <imhendrick16@gmail.com>
2025-02-01 20:53:50 -0500

    update: authorize user request

--- app/Http/Requests/Api/V1/UpdateUserRequest.php
+++ app/Http/Requests/Api/V1/UpdateUserRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests\Api\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateUserRequest extends FormRequest
+class UpdateUserRequest extends BaseUserRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'data.attributes.name' => 'sometimes|string|max:255|min:3',
+            'data.attributes.email' => 'sometimes|string|email|unique:users,email',
+            'data.attributes.password' => 'sometimes|string|min:8|max:50|',
+            'data.attributes.default_currency' => 'sometimes|string|size:3',
+            'data.attributes.is_notifiable' => 'sometimes|boolean',
         ];
     }
 }
