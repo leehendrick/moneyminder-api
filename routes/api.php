fe0bc7202dd3706a9403f95361a2a6698d500401
leehendrick <imhendrick16@gmail.com>
2025-02-04 16:55:49 -0500

    rebase local

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::get('/home', function (Request $request) {
+
+Route::get('/', function (Request $request) {
     return view('welcome');
 });
 Route::post('/login', [AuthController::class, 'login']);
