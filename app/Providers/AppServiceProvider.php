664f9286a918ffc2ec82a361704426701c950fa6
leehendrick <imhendrick16@gmail.com>
2025-02-04 01:29:51 -0500

    rename policies gate name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,18 +24,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('update', [UserPolicy::class, 'update']);
-        Gate::define('replace', [UserPolicy::class, 'replace']);
-        Gate::define('delete', [UserPolicy::class, 'delete']);
+        Gate::define('update-user', [UserPolicy::class, 'update']);
+        Gate::define('replace-user', [UserPolicy::class, 'replace']);
+        Gate::define('delete-user', [UserPolicy::class, 'delete']);
 
-        Gate::define('update', [CategoryPolicy::class, 'update']);
-        Gate::define('replace', [CategoryPolicy::class, 'replace']);
-        Gate::define('create', [CategoryPolicy::class, 'create']);
-        Gate::define('delete', [CategoryPolicy::class, 'delete']);
+        Gate::define('update-category', [CategoryPolicy::class, 'update']);
+        Gate::define('replace-category', [CategoryPolicy::class, 'replace']);
+        Gate::define('create-category', [CategoryPolicy::class, 'create']);
+        Gate::define('delete-category', [CategoryPolicy::class, 'delete']);
 
-        Gate::define('update', [TransactionTypePolicy::class, 'update']);
-        Gate::define('replace', [TransactionTypePolicy::class, 'replace']);
-        Gate::define('create', [TransactionTypePolicy::class, 'create']);
-        Gate::define('delete', [TransactionTypePolicy::class, 'delete']);
+        Gate::define('update-transaction-type', [TransactionTypePolicy::class, 'update']);
+        Gate::define('replace-transaction-type', [TransactionTypePolicy::class, 'replace']);
+        Gate::define('create-transaction-type', [TransactionTypePolicy::class, 'create']);
+        Gate::define('delete-transaction-type', [TransactionTypePolicy::class, 'delete']);
     }
 }
