7589b30b9c7a9bd5cf5db17efdefc58c84dc80a8
Hendrick Nkuba <115194843+leehendrick@users.noreply.github.com>
2025-02-04 09:32:45 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
+Route::get('/', function (Request $request) {
+    return view('welcome');
+});
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
